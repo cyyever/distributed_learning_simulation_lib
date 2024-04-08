a40f29a5c7bf6cdece6b0d1f146b7e8e356deae5
cyy <cyyever@outlook.com>
2024-04-08 15:39:38 +0800

    Add import

--- simulation_lib/__init__.py
+++ simulation_lib/__init__.py
@@ -1,4 +1,5 @@
 from .algorithm import *  # noqa: F401
+from .algorithm_factory import CentralizedAlgorithmFactory  # noqa: F401
 from .message import *  # noqa: F401
 from .server import *  # noqa: F401
 from .worker import *  # noqa: F401
