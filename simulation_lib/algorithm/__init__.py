9c7be36c777dab848641fd1cf9cb1967a56b57c9
cyy <cyyever@outlook.com>
2024-04-08 15:51:05 +0800

    Fix location

--- simulation_lib/algorithm/__init__.py
+++ simulation_lib/algorithm/__init__.py
@@ -2,8 +2,7 @@ from .aggregation_algorithm import AggregationAlgorithm  # noqa: F401
 from .composite_aggregation_algorithm import \
     CompositeAggregationAlgorithm  # noqa: F401
 from .fed_avg_algorithm import FedAVGAlgorithm  # noqa: F401
-from .graph.graph_algorithm import GraphAlgorithm  # noqa: F401
-from .graph.graph_embedding_algorithm import \
+from .graph_algorithm import GraphAlgorithm  # noqa: F401
+from .graph_embedding_algorithm import \
     GraphNodeEmbeddingPassingAlgorithm  # noqa: F401
-from .graph.graph_topology_algorithm import \
-    GraphTopologyAlgorithm  # noqa: F401
+from .graph_topology_algorithm import GraphTopologyAlgorithm  # noqa: F401
