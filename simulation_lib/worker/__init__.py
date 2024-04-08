2f810e00203000a9edd51f49b1eabba2cf105baa
cyy <cyyever@outlook.com>
2024-04-08 15:52:48 +0800

    Fix location

--- simulation_lib/worker/__init__.py
+++ simulation_lib/worker/__init__.py
@@ -1,5 +1,5 @@
 from .aggregation_worker import AggregationWorker  # noqa: F401
 from .error_feedback_worker import ErrorFeedbackWorker  # noqa: F401
 from .gradient_worker import GradientWorker  # noqa: F401
-from .graph.graph_worker import GraphWorker  # noqa: F401
-from .graph.node_selection_worker import NodeSelectionMixin  # noqa: F401
+from .graph_worker import GraphWorker  # noqa: F401
+from .node_selection_worker import NodeSelectionMixin  # noqa: F401
