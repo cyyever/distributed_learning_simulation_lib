e68a6b7f8056b4de298aaaf7f362c8187a9af89b
cyy <cyyever@outlook.com>
2024-04-09 15:03:16 +0800

    Fix code

--- simulation_lib/worker/__init__.py
+++ simulation_lib/worker/__init__.py
@@ -2,6 +2,7 @@ from ..dependency import import_results
 from .aggregation_worker import AggregationWorker  # noqa: F401
 from .error_feedback_worker import ErrorFeedbackWorker  # noqa: F401
 from .gradient_worker import GradientWorker  # noqa: F401
+from .protocol import *  # noqa: F401
 
 if "cyy_torch_graph" in import_results:
     from .graph_worker import GraphWorker  # noqa: F401
