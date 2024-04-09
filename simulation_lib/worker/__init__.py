810c7ee8c21cca9c6aac84adf41dfa428df21159
cyy <cyyever@outlook.com>
2024-04-09 12:03:03 +0800

    Improve dependency detection

--- simulation_lib/worker/__init__.py
+++ simulation_lib/worker/__init__.py
@@ -1,5 +1,8 @@
+from ..dependency import import_results
 from .aggregation_worker import AggregationWorker  # noqa: F401
 from .error_feedback_worker import ErrorFeedbackWorker  # noqa: F401
 from .gradient_worker import GradientWorker  # noqa: F401
-from .graph_worker import GraphWorker  # noqa: F401
-from .node_selection_worker import NodeSelectionMixin  # noqa: F401
+
+if "cyy_torch_graph" in import_results:
+    from .graph_worker import GraphWorker  # noqa: F401
+    from .node_selection_worker import NodeSelectionMixin  # noqa: F401
