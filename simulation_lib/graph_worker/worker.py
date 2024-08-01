e28fca960b3b788e1bf814aec6612d8abbb3dc78
cyy <cyyever@outlook.com>
2024-08-01 09:43:38 +0800

    Fix code

--- simulation_lib/graph_worker/worker.py
+++ simulation_lib/graph_worker/worker.py
@@ -225,7 +225,7 @@ class GraphWorker(AggregationWorker):
         edge_index = torch_geometric.utils.coalesce(edge_index[:, edge_mask])
         self.trainer.mutable_dataset_collection.transform_dataset(
             self.trainer.phase,
-            lambda _, dataset_util, __: dataset_util.get_edge_subset(
+            lambda dataset_util: dataset_util.get_edge_subset(
                 graph_index=0, edge_index=edge_index
             ),
         )
@@ -234,7 +234,7 @@ class GraphWorker(AggregationWorker):
         edge_index = self.__get_local_edge_index()
         self.trainer.mutable_dataset_collection.transform_dataset(
             self.trainer.phase,
-            lambda _, dataset_util, __: dataset_util.get_edge_subset(
+            lambda dataset_util: dataset_util.get_edge_subset(
                 graph_index=0, edge_index=edge_index
             ),
         )
