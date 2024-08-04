4684a4f3edec70587d3b7467b83db096e6ac7997
cyy <cyyever@outlook.com>
2024-08-04 14:36:33 +0800

    Fix code

--- simulation_lib/server/round_selection_mixin.py
+++ simulation_lib/server/round_selection_mixin.py
@@ -4,12 +4,20 @@ from .protocol import AggregationServerProtocol
 
 
 class RoundSelectionMixin(AggregationServerProtocol):
-    def select_workers(self) -> set:
+    selection_result: dict[int, set[int]] = {}
+
+    def select_workers(self) -> set[int]:
+        if self.round_index in self.selection_result:
+            return self.selection_result[self.round_index]
         random_client_number: int | None = self.config.algorithm_kwargs.pop(
             "random_client_number", None
         )
+        result: set[int] = set()
         if random_client_number is not None:
-            return set(
+            result = set(
                 random.sample(list(range(self.worker_number)), k=random_client_number)
             )
-        return set(range(self.worker_number))
+        else:
+            result = set(range(self.worker_number))
+        self.selection_result[self.round_index] = result
+        return result
