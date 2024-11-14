13bc375d23bc0a0407825e3da039133bb22f8bb0
cyy <cyyever@outlook.com>
2024-11-14 10:37:14 +0800

    Fix deps

--- simulation_lib/algorithm/__init__.py
+++ simulation_lib/algorithm/__init__.py
@@ -1,9 +1,15 @@
 from .aggregation_algorithm import AggregationAlgorithm  # noqa: F401
 from .composite_aggregation_algorithm import CompositeAggregationAlgorithm  # noqa: F401
 from .fed_avg_algorithm import FedAVGAlgorithm  # noqa: F401
-from .graph_algorithm import GraphAlgorithm  # noqa: F401
-from .graph_embedding_algorithm import GraphNodeEmbeddingPassingAlgorithm  # noqa: F401
-from .graph_topology_algorithm import GraphTopologyAlgorithm  # noqa: F401
+
+try:
+    from .graph_algorithm import GraphAlgorithm  # noqa: F401
+    from .graph_embedding_algorithm import (
+        GraphNodeEmbeddingPassingAlgorithm,  # noqa: F401
+    )
+    from .graph_topology_algorithm import GraphTopologyAlgorithm  # noqa: F401
+except Exception:
+    pass
 from .personalized_aggregation_algorithm import (
     PersonalizedFedAVGAlgorithm,  # noqa: F401
 )
