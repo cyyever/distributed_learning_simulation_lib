e288d2d29825fef4f3e85e47b56cea405f57c739
cyy <cyyever@outlook.com>
2024-11-14 10:34:45 +0800

    Fix deps

--- simulation_lib/topology/__init__.py
+++ simulation_lib/topology/__init__.py
@@ -1,2 +1,6 @@
 from .dp_endpoint import DifferentialPrivacyEmbeddingEndpoint  # noqa: F401
-from .quantized_endpoint import *  # noqa: F401
+
+try:
+    from .quantized_endpoint import *  # noqa: F401
+except Exception:
+    pass
