8642283a405f9af1a1e7b711e71af7dbb2251af5
cyy <cyyever@outlook.com>
2024-12-01 11:36:10 +0800

    Fix code

--- simulation_lib/topology/__init__.py
+++ simulation_lib/topology/__init__.py
@@ -7,11 +7,15 @@ with contextlib.suppress(Exception):
     from .quantized_endpoint import (
         NNADQClientEndpoint,
         NNADQServerEndpoint,
+        QuantClientEndpoint,
+        QuantServerEndpoint,
         StochasticQuantClientEndpoint,
         StochasticQuantServerEndpoint,
     )
 
     __all__ += [
+        "QuantServerEndpoint",
+        "QuantClientEndpoint",
         "StochasticQuantClientEndpoint",
         "StochasticQuantServerEndpoint",
         "NNADQClientEndpoint",
