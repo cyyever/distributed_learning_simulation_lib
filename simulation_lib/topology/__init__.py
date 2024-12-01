593810abf7f1b2ef5646080912975db6277dfe6a
cyy <cyyever@outlook.com>
2024-12-01 10:26:01 +0800

    Fix code

--- simulation_lib/topology/__init__.py
+++ simulation_lib/topology/__init__.py
@@ -1,6 +1,19 @@
 import contextlib
 
-from .dp_endpoint import DifferentialPrivacyEmbeddingEndpoint  # noqa: F401
+from .dp_endpoint import DifferentialPrivacyEmbeddingEndpoint
 
+__all__ = ["DifferentialPrivacyEmbeddingEndpoint"]
 with contextlib.suppress(Exception):
-    from .quantized_endpoint import *  # noqa: F401
+    from .quantized_endpoint import (
+        NNADQClientEndpoint,
+        NNADQServerEndpoint,
+        StochasticQuantClientEndpoint,
+        StochasticQuantServerEndpoint,
+    )
+
+    __all__ += [
+        "StochasticQuantClientEndpoint",
+        "StochasticQuantServerEndpoint",
+        "NNADQClientEndpoint",
+        "NNADQServerEndpoint",
+    ]
