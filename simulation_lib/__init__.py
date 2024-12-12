590c2f803a45023bb10d4e261fd86f9b0cce992b
cyy <cyyever@outlook.com>
2024-12-12 10:14:44 +0800

    Add code

--- simulation_lib/__init__.py
+++ simulation_lib/__init__.py
@@ -1,3 +1,6 @@
+from cyy_naive_lib.log import set_multiprocessing_ctx
+from cyy_torch_toolbox import TorchProcessContext
+
 from .algorithm import *  # noqa: F401
 from .algorithm_factory import CentralizedAlgorithmFactory  # noqa: F401
 from .config import *  # noqa: F401
@@ -6,3 +9,5 @@ from .server import *  # noqa: F401
 from .topology import *  # noqa: F401
 from .util import *  # noqa: F401
 from .worker import *  # noqa: F401
+
+set_multiprocessing_ctx(TorchProcessContext().get_ctx())
