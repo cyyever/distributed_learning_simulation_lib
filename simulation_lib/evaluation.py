d0cfb9c25577b3c37df727026c29bedc6e980fa8
cyy <cyyever@outlook.com>
2025-02-12 15:24:25 +0800

    Fix code

--- simulation_lib/evaluation.py
+++ simulation_lib/evaluation.py
@@ -3,7 +3,7 @@ import os
 
 from .config import DistributedTrainingConfig
 from .server import Server
-from .task import create_server, get_task_config
+from .task import create_server, get_server_config
 
 # we use these environment variables to save memory in large-scale training
 os.environ["CUDA_MODULE_LOADING"] = "LAZY"
@@ -13,9 +13,7 @@ os.environ["USE_THREAD_DATALOADER"] = "1"
 def get_server(config: DistributedTrainingConfig) -> Server:
     # we need to deepcopy config for concurrent training
     config = copy.deepcopy(config)
-    if config.worker_number == 0:
-        config.worker_number = 1
     config.reset_session()
     config.apply_global_config()
-    task_config = get_task_config(config)
+    task_config = get_server_config(config)
     return create_server(task_config=task_config, context=task_config["context"])
--- simulation_lib/task.py
+++ simulation_lib/task.py
@@ -11,17 +11,36 @@ from .server import Server
 from .task_type import TaskIDType
 
 type TaskConfig = dict
+type TaskServerConfig = dict
 
 
 def get_task_id() -> TaskIDType:
     return uuid.uuid4()
 
 
+def get_server_config(config: DistributedTrainingConfig) -> TaskServerConfig:
+    assert AlgorithmRepository.has_algorithm(config.distributed_algorithm)
+    context = FederatedLearningContext(worker_num=config.worker_number)
+    task_id = get_task_id()
+    result: dict = {"context": context, "task_id": task_id}
+    result["server"] = {}
+    result["server"]["constructor"] = functools.partial(
+        AlgorithmRepository.create_server,
+        algorithm_name=config.distributed_algorithm,
+        endpoint_kwargs=config.endpoint_kwargs.get("server", {}),
+        kwargs={
+            "config": config,
+            "task_id": task_id,
+        },
+    )
+    return result
+
+
 def get_task_config(
     config: DistributedTrainingConfig,
     practitioners: None | set = None,
 ) -> TaskConfig:
-    task_id = get_task_id()
+    result = get_server_config(config=config)
     if practitioners is None:
         practitioners = config.create_practitioners()
     else:
@@ -34,20 +53,6 @@ def get_task_config(
     assert practitioners
     assert AlgorithmRepository.has_algorithm(config.distributed_algorithm)
 
-    context = FederatedLearningContext(worker_num=config.worker_number)
-    result: dict = {
-        "context": context,
-    }
-    result["server"] = {}
-    result["server"]["constructor"] = functools.partial(
-        AlgorithmRepository.create_server,
-        algorithm_name=config.distributed_algorithm,
-        endpoint_kwargs=config.endpoint_kwargs.get("server", {}),
-        kwargs={
-            "config": config,
-            "task_id": task_id,
-        },
-    )
     worker_number_per_process = config.get_worker_number_per_process()
     log_warning(
         "There are %s workers in total, and %s workers form a group",
@@ -68,7 +73,7 @@ def get_task_config(
                         },
                         kwargs={
                             "config": config,
-                            "task_id": task_id,
+                            "task_id": result["task_id"],
                             "practitioner": practitioner,
                         },
                     ),
@@ -78,11 +83,10 @@ def get_task_config(
         )
     assert client_config
     result["worker"] = client_config
-    result["task_id"] = task_id
     return result
 
 
-def create_server(task_config: TaskConfig, **kwargs) -> Server:
+def create_server(task_config: dict, **kwargs) -> Server:
     if "context" not in kwargs:
         kwargs["context"] = task_config["context"]
     return task_config["server"]["constructor"](**kwargs)
